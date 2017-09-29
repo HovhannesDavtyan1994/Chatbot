49833d7545a7c90635d1dbfb5da6afb49c44ccf3
HovhannesDavtyan1994 <hovhannes@benivo.com>
2017-09-29 15:44:31 +0400

    infographic 3

--- src/app/Infographics/infographic3/infographic3.component.ts
+++ src/app/Infographics/infographic3/infographic3.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Chart from 'chart.js';
 
 @Component({
   selector: 'app-infographic3',
@@ -10,6 +11,59 @@ export class Infographic3Component implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    var canvas1 = <HTMLCanvasElement> document.getElementById("chart1");
+    var ctx1 = canvas1.getContext("2d");
+    var canvas2 = <HTMLCanvasElement> document.getElementById("chart2");
+    var ctx2 = canvas2.getContext("2d");
+    var Chart1 = new Chart(ctx1, {
+        type: 'pie',
+        data: {
+          datasets: [{
+              data: [
+                  17,
+                  83
+              ],
+              backgroundColor: [
+                  "rgba(22, 134, 255, 1)",
+                  "rgba(193, 39, 45, 1)"
+              ],
+              label: 'Dataset 1'
+          }],
+          labels: [
+              "Room",
+              "1 bed flat"
+          ]
+      },
+      options: {
+          responsive: true
+      }
+    });
+    var Chart2 = new Chart(ctx2, {
+      type: 'pie',
+      data: {
+        datasets: [{
+            data: [
+                10,
+                72,
+                18
+            ],
+            backgroundColor: [
+                "rgba(247, 147, 30, 1)",
+                "rgba(22, 134, 255, 1)",
+                "rgba(193, 39, 45, 1)"
+            ],
+            label: 'Dataset 1'
+        }],
+        labels: [
+            "Walk",
+            "Tube",
+            "Bike"
+        ]
+      },
+      options: {
+          responsive: true
+      }
+    });
   }
 
 }
