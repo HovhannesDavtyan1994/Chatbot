57c145706da38b580a8ec46db7e91ad62def5cbf
Scylla888 <arsen.asulyan.1991@gmail.com>
2017-09-29 18:55:11 +0400

    infographic 5 component and binding

--- src/app/Infographics/infographic5/infographic5.component.ts
+++ src/app/Infographics/infographic5/infographic5.component.ts
@@ -7,6 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Infographic5Component implements OnInit {
 
+  public Employee = {
+    AreaName : "Mile End",
+    TransportToOffice : {
+      Bike:{
+        ColleaguesPercent : "72",
+        Price:"135",
+        Time : "20"
+      },
+      Cycle:{
+        ColleaguesPercent : "18",
+        Price:"60*",
+        Time : "15"
+      },
+      Walk:{
+        ColleaguesPercent : "10",
+        Price:"0",
+        Time : "50"
+      }
+    }
+  }
   constructor() { }
 
   ngOnInit() {
