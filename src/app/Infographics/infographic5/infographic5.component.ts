b70fa8aa4833a135e34a73967f077f95a7c2d018
Scylla888 <arsen.asulyan.1991@gmail.com>
2017-10-03 14:42:30 +0400

    mapping

--- src/app/Infographics/infographic5/infographic5.component.ts
+++ src/app/Infographics/infographic5/infographic5.component.ts
@@ -8,26 +8,7 @@ import { Component, OnInit,Input } from '@angular/core';
 export class Infographic5Component implements OnInit {
   @Input() model: any
   public data = this.model;
-  public Employee = {
-    AreaName : "Mile End",
-    TransportToOffice : {
-      Bike:{
-        ColleaguesPercent : "72",
-        Price:"135",
-        Time : "20"
-      },
-      Cycle:{
-        ColleaguesPercent : "18",
-        Price:"60*",
-        Time : "15"
-      },
-      Walk:{
-        ColleaguesPercent : "10",
-        Price:"0",
-        Time : "50"
-      }
-    }
-  }
+ 
   constructor() { }
 
   ngOnInit() {
--- src/app/Infographics/infographic7/infographic7.component.ts
+++ src/app/Infographics/infographic7/infographic7.component.ts
@@ -8,28 +8,28 @@ import { Component, OnInit ,Input} from '@angular/core';
 export class Infographic7Component implements OnInit {
   @Input() model: any
   public data = this.model;
-  public Employee = {
-    Name : "Emma",
-    MovingCity : "London",
-    Price : {
-      Rent : {
-        Cost : "700",
-        euro : "770"
-      },
-      Commute : {
-        Cost : "135",
-        euro : "145"
-      },
-      Groceries : {
-        Cost : "145",
-        euro : "160"
-      },
-      Total : {
-        Cost : "1.480",
-        euro : "1.625"
-      }
-    }
-  }
+  // public Employee = {
+  //   Name : "Emma",
+  //   MovingCity : "London",
+  //   Price : {
+  //     Rent : {
+  //       Cost : "700",
+  //       euro : "770"
+  //     },
+  //     Commute : {
+  //       Cost : "135",
+  //       euro : "145"
+  //     },
+  //     Groceries : {
+  //       Cost : "145",
+  //       euro : "160"
+  //     },
+  //     Total : {
+  //       Cost : "1.480",
+  //       euro : "1.625"
+  //     }
+  //   }
+  // }
   constructor() { }
 
   ngOnInit() {
